db555edb26199961ea69b278e8a2806314305ff3
RuoChen95 <xrc_1995@163.com>
2019-03-02 17:52:04 +0800

    使用顶点+边的数据格式

--- week #2/dijkstraAlgorithms.js
+++ week #2/dijkstraAlgorithms.js
@@ -3,67 +3,49 @@ let rs = require("fs");
 // let data = rs.readFileSync("dijkstraData.txt","utf-8");
 let data = rs.readFileSync("testCase.txt","utf-8");
 
-let V = data.split('\n');
+let array = data.split('\n');
 
-for (let i = 0; i < V.length; i++) {
-  V[i] = V[i].split('\t').filter(n => {
-    if (n !== '\r') return n
-  }).map(n => {
-    return n.split(',').map(n => {
-      return parseInt(n)
-    })
-  })
+for (let i = 0; i < array.length; i++) {
+  array[i] = array[i].split('\t')
 }
 
-V.unshift(undefined)
+// for (let i = 0; i < V.length; i++) {
+//   V[i] = V[i].split('\t').filter(n => {
+//     if (n !== '\t') return n
+//   }).map(n => {
+//     return n.split(',').map(n => {
+//       return parseInt(n)
+//     })
+//   })
+// }
 
-let X = [];
-let A = [undefined, 0];
 
-X.push(V[1]);
+console.log('array', array)
 
-let w = undefined
-
-X.forEach(function(n) {
-  let v = n[0][0];
-
-  let wIndex = 1;
-  let w = n[wIndex][0];
-
-  let l_vw = A[v] + n[1][1];
-  n.forEach(function(m, index) {
-    if (index >= 2) {
-      if (l_vw > A[v] + getLength(m)) {
-        l_vw = A[v] + getLength(m);
-        wIndex = index
-      }
+let inputVertices = array.map(function (n) {
+  return parseInt(n[0])
+});
+let inputEdges = [];
+array.forEach(function (n) {
+  n.forEach(function (m, index, array) {
+    // 数组是引用类型
+    let mArray = m.split(',')
+    if (index >= 1 && !inputEdges.some(function (arr) {
+      return JSON.stringify(arr) === JSON.stringify([mArray[0], array[0], mArray[1]])
+    })) {
+      inputEdges.push([array[0], mArray[0], mArray[1]])
     }
   })
-
-
-  // console.log(l_vw)
-  // console.log(wIndex)
-
-  w = n[wIndex][0]
-  A[w] = A[v] + l_vw
+});
+inputEdges = inputEdges.map(function (n) {
+  return n.map(function (m) {
+    return parseInt(m)
+  })
 })
+console.log(inputVertices)
+console.log(inputEdges)
 
-X.push(V[w])
-// console.log(X)
-// console.log(A)
-
-
-X.forEach(function(n) {
-  console.log(n)
-
-  let v = n[0][0]
-
-  let l_vw = A[v] + n[1][1]
-
-  console.log(l_vw)
-})
-
-
-function getLength(n) {
-  return n[1]
-}
+/*
+**** Dijkstra's Algorithm ****
+*
+*/
